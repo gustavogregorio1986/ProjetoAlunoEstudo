70885dc1df9c784bcffa9238e876c9312eee2c66
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-04-09 14:44:50 -0300

    Desenvolvi a classe AlunoView do projeto do sistema aluno

---
+++ ProjetoAluno/Models/AlunoView.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjetoAluno.Models
+{
+    public class AlunoView
+    {
+        public Guid Id { get; set; }
+
+        public string? NomeAluno { get; set; }
+
+        public double Nota1 { get; set; }
+
+        public double Nota2 { get; set; }
+
+        public double TotalNota { get; set; }
+
+        public string? Situacao { get; set; }
+
+        public DateTime DataInicio { get; set; }
+
+        public DateTime DataTermino { get; set; }
+
+        public string? Escolareidade { get; set; }
+
+        public string? Status { get; set; }
+    }
+}
