f558a3a0e81390692d7d3139bd26f10917f8e325
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-04-09 17:22:46 -0300

    desenvolvendo o cadastro completo com suas validações e a mensagem de gravado com sucesso!

--- ProjetoAluno/Controllers/AlunoController.cs
+++ ProjetoAluno/Controllers/AlunoController.cs
@@ -1,13 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoAluno.Dominio.Dominio;
+using ProjetoAluno.Models;
+using ProjetoAluno.Service.Service.Interface;
 
 namespace ProjetoAluno.Controllers
 {
     public class AlunoController : Controller
     {
+        private readonly IAlunoService _alunoService;
+
+        public AlunoController(IAlunoService alunoService)
+        {
+            _alunoService = alunoService;
+        }
+
         public IActionResult Cadastrar()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Cadastrar(AlunoView model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var aluno = new Aluno
+                    {
+                        NomeAluno = model.NomeAluno,
+                        Nota1 = model.Nota1,
+                        Nota2 = model.Nota2,
+                        TotalNota = model.TotalNota,
+                        Situacao = model.Situacao,
+                        DataInicio = model.DataInicio,
+                        DataTermino = model.DataTermino,
+                        Escolaridade = model.Escolaridade,
+                        Status = model.Status
+                    };
+
+                    _alunoService.Adicionar(aluno);
+                    TempData["MensagemSucesso"] = "Aluno cadastrado com sucesso!";
+                    return RedirectToAction("Cadastrar");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["MensagemErro"] = "Erro ao cadastrar aluno.";
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+
+            return View(model); // volta com os dados preenchidos
+        }
+
 
         public IActionResult Consultar()
         {
--- ProjetoAluno/Models/AlunoView.cs
+++ ProjetoAluno/Models/AlunoView.cs
@@ -1,25 +1,40 @@
-﻿namespace ProjetoAluno.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoAluno.Models
 {
     public class AlunoView
     {
         public Guid Id { get; set; }
 
+        [Display(Name = "Nome do ALuno")]
+        [Required(ErrorMessage = "Informe o Nome do Aluno")]
         public string? NomeAluno { get; set; }
 
+        [Required(ErrorMessage = "Informe o Nota1 do Aluno")]
         public double Nota1 { get; set; }
 
+        [Required(ErrorMessage = "Informe o Nota2 do Aluno")]
         public double Nota2 { get; set; }
 
+        [Display(Name = "Total Media")]
+        [Required(ErrorMessage = "Informe a media do Aluno")]
         public double TotalNota { get; set; }
 
+        [Required(ErrorMessage = "Informe a situação do Aluno")]
         public string? Situacao { get; set; }
 
+        [Display(Name = "Data de Inicio")]
+        [Required(ErrorMessage = "Informe a Data de Inicio do Aluno")]
         public DateTime DataInicio { get; set; }
 
+        [Display(Name = "Data de Termino")]
+        [Required(ErrorMessage = "Informe a data de termino do Aluno")]
         public DateTime DataTermino { get; set; }
 
+        [Required(ErrorMessage = "Informe a escolaridade do Aluno")]
         public string? Escolaridade { get; set; }
 
+        [Required(ErrorMessage = "Informe o status do Aluno")]
         public string? Status { get; set; }
     }
 }
